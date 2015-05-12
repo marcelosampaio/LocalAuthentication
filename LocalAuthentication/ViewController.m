a082421ea1f87abf820421a6a739929e1b4f0041
Marcelo Sampaio <marcelo@artlogica.com.br>
2015-05-12 10:11:57 -0300

    Initial commit

--- LocalAuthentication/ViewController.m
+++ LocalAuthentication/ViewController.m
@@ -5,6 +5,7 @@
 //  Created by Marcelo Sampaio on 5/12/15.
 //  Copyright (c) 2015 Marcelo Sampaio. All rights reserved.
 //
+//  Reference: https://www.secsign.com/fingerprint-validation-as-an-alternative-to-passcodes/
 
 #import "ViewController.h"
 
@@ -12,16 +13,36 @@
 
 @end
 
+@import LocalAuthentication;
+
 @implementation ViewController
 
+#pragma mark - View Life Cycle
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Get the local authentication context:
+    LAContext *context = [[LAContext alloc] init];
+    
+    // Test if fingerprint authentication is available on the device and a fingerprint has been enrolled.
+    if ([context canEvaluatePolicy: LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil])
+    {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:@"Authenticate for server login" reply:^(BOOL success, NSError *authenticationError){
+            if (success) {
+                NSLog(@"Fingerprint validated.");
+            }
+            else {
+                NSLog(@"Fingerprint validation failed: %@.", authenticationError.localizedDescription);
+            }
+        }];
+    }else{
+        NSLog(@"NO");
+    }
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
+
+
+
 
 @end
